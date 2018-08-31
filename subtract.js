75cb579f4181ea50f432769b3b5e74b4790e3582
ishan <ishangupta25@gmail.com>
2018-08-31 14:43:02 +0530

    delete

---
+++ subtract.js
@@ -0,0 +1,10 @@
+// subtract-1.js
+// return subtraction of two numbers
+function subtract(a, b, bMINUSa) {
+    if (bMINUSa === true) {
+        return b - a;
+    }
+    else {
+        return a - b;
+    }
+};
